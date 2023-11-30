33c5be64bf3ae69f7e81b922a7423e1de094d8e4
Artek60744 <48151951+Artek60744@users.noreply.github.com>
2023-11-30 10:00:34 +0100

    TD1 Exercice1-4
    Il reste la question 5 de l'exercice 4 à faire

---
+++ TD1/src/Point.java
@@ -0,0 +1,60 @@
+package src;
+// Fichier Point.java 
+public class Point { 
+	// deux attributs de type int 
+	int abscisse; 
+	int ordonnee;
+
+	// constructeur 
+	Point(){ 
+		abscisse = 0 ;
+		ordonnee = 0 ;
+	}
+
+	// constructeur 2
+	Point(int u, int v){ 
+		this.abscisse = u ;
+		this.ordonnee = v ;
+	}
+
+    void affiche(){
+        System.out.println("Abscisse:"+this.abscisse+" Ordonnée:"+this.ordonnee);
+    }
+
+	// méthode permettant de changer les coordonnées d’un point 
+	void set( int u , int v ){
+		abscisse = u ;
+		ordonnee = v ;
+	}
+
+	// Méthode set qui copy les attribues de l'object 2 dans notre object
+	void set( Point p2){
+		this.abscisse = p2.abscisse ;
+		this.ordonnee = p2.ordonnee ;
+	}
+
+    // Affiche 
+    boolean origine(){
+        if (this.abscisse == 0 || this.ordonnee == 0)
+            return true;
+        return false;
+    }
+
+    boolean egale(Point point2){
+        if (this.abscisse == point2.abscisse && this.ordonnee == point2.ordonnee)
+            return true;
+        return false;
+    }
+
+	// méthode permettant de translater un point
+	void translate( int u , int v ){
+		abscisse = abscisse + u ; 
+		ordonnee = ordonnee + v ; 
+	}
+
+	Point symetrie(){
+		this.abscisse = -this.abscisse;
+		this.ordonnee = -this.ordonnee;
+		return this;
+	}
+}
---
+++ TD1/src/Teste.java
@@ -0,0 +1,30 @@
+package src;
+/**
+ * main
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        Point p = new Point();
+        Point p2 = new Point();
+        p2.set(2, 3);
+        p.affiche();
+        p2.affiche();
+        
+        // return true si le point est sur (0, 0)
+        System.out.println("Le point p est sur l'origine ?"+p.origine());
+        System.out.println("Le point p2 est sur l'origine ?"+p2.origine());
+
+        System.out.println("Le point p est égale à p2 ?"+p.egale(p2));
+
+        Point p3 = new Point(2, 3);
+        System.out.println("Le point 3:");
+        p3.affiche();
+        System.out.println("Le point p3 est égale à p2 ?"+p2.egale(p3));
+        
+        p3.symetrie();
+        System.out.println("Symétrie du point3 par rapport à l'origine");
+        p3.affiche();
+
+    }
+}
